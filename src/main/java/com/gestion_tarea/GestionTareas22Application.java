aaec1c9c3fbd0ce16de9afec1b39dcf14a2a9a0b
luis <luisfelizbeltre@gmail.com>
2025-03-14 17:01:07 +0100

    djnksk

--- src/main/java/com/gestion_tarea/GestionTareas22Application.java
+++ src/main/java/com/gestion_tarea/GestionTareas22Application.java
@@ -2,8 +2,10 @@ package com.gestion_tarea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@EnableJpaRepositories("com.gestion_tarea.repository")
 @SpringBootApplication
+
 public class GestionTareas22Application {
 
 	public static void main(String[] args) {
