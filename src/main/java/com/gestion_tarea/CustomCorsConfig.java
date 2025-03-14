89a217e53d2494174f92ef5bd55a26f16c9606cb
luis <luisfelizbeltre@gmail.com>
2025-03-14 15:25:32 +0100

    Guardar cambios en CustomCorsConfig.java

--- src/main/java/com/gestion_tarea/CustomCorsConfig.java
+++ src/main/java/com/gestion_tarea/CustomCorsConfig.java
@@ -17,6 +17,7 @@ public class CustomCorsConfig {
         config.addAllowedOrigin("http://192.168.100.137:8081");
         config.addAllowedOrigin("http://192.168.100.128:8081");
         config.addAllowedOrigin("http://23.20.232.178");  // Cambiar el origen a la IP del servidor
+        config.addAllowedOrigin("http://ec2-23-20-232-178.compute-1.amazonaws.com");  // Cambiar el origen a la IP del servidor
 
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
